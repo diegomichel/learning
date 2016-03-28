a8969809d1d59ab978e659b285fd90bc7d01c596
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-03-27 23:35:00 -0600

    Use of constants to list constants

--- metaprogramming/monday/absolute_path_test.rb
+++ metaprogramming/monday/absolute_path_test.rb
@@ -7,4 +7,7 @@ class AbsolutePathTest < Test::Unit::TestCase
   def test_absolute_path
     assert_equal('hai', AbsolutePath::MyClass.new.get_constant)
   end
+  def test_constants
+    assert_equal([:MyClass, :Y], AbsolutePath.constants)
+  end
 end
