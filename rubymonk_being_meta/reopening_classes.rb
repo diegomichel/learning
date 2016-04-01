3b27cf2f9e5c05973acb77d9ca1188a44950f1fd
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-03-31 22:24:25 -0600

    Explained inject method and send

--- rubymonk_being_meta/reopening_classes.rb
+++ rubymonk_being_meta/reopening_classes.rb
@@ -1,6 +1,15 @@
-class Array
-  def foldl(method)
+class Array #We are opening the existing Array class
+  def foldl(method) #We are defining a new method on the class
+    #Inject combines all the elements of the array
+    #result is an acumulator that stores the last return value of
+    #the iteration.
+    #the param i of the block is each element of the array.
+    #Also the acumulator is returned at the very end.
     inject {|result, i| result ? result.send(method, i) : i}
+    #So first check if result is defined, if not then the next iteration
+    #result will have the value of i.
+    #now result is defined, so we call method on it, and as a param the next
+    #value of the array, and so on, until the end...
   end
 end
 
