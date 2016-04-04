3a2aeaa984e0bbbb681d43becba2b31b16781c82
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-04-03 22:44:57 -0500

    More about eval

--- rubymonk_being_meta/eval.rb
+++ rubymonk_being_meta/eval.rb
@@ -4,3 +4,9 @@ answer = 5
 answer < eval("answer + 1")
 eval("class Hi; def hello; p 'Hello !'; end; end")
 Hi.new.hello
+
+contents = File.open('zen', 'r')
+result = eval("contents.read")
+# Be careful eval is considered evil...
+# Never pass to eval any input from outside
+p eval("ls //")
