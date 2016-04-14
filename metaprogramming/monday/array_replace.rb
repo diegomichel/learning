63691fece4b19e3dd84529f0695ccd0629b99997
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-04-13 22:38:14 -0500

    Demostrates the dark side of open classes

---
+++ metaprogramming/monday/array_replace.rb
@@ -0,0 +1,6 @@
+class Array
+  def replace
+    #This will make my test fail, this is called monkey patching.
+    #This is the dark side of open classes
+  end
+end
--- metaprogramming/monday/array_replace_test.rb
+++ metaprogramming/monday/array_replace_test.rb
@@ -1,5 +1,8 @@
 require 'test/unit'
 class TestArrayReplace < Test::Unit::TestCase
+  def setup
+    load 'array_replace.rb'
+  end
   def test_array_replace
     assert_equal([1,2,3], [].replace([1,2,3]))
   end
