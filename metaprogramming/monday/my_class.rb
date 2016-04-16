55508a92e2df5512ce3a06b73c1b33facb2f9288
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-04-15 21:41:37 -0500

    Test instance_variables of an object

--- metaprogramming/monday/my_class.rb
+++ metaprogramming/monday/my_class.rb
@@ -1,2 +1,5 @@
 class MyClass
+  def my_method
+    @v = 1
+  end
 end
--- metaprogramming/monday/whats_an_object_test.rb
+++ metaprogramming/monday/whats_an_object_test.rb
@@ -7,4 +7,9 @@ class TestWhatsAnObject < Test::Unit::TestCase
   def test_class
     assert_equal(MyClass, @obj.class)
   end
+  def test_instance_variables
+    assert_equal(0, @obj.instance_variables.count)
+    @obj.my_method
+    assert_equal(:@v, @obj.instance_variables.first)
+  end
 end
