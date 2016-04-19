f65eeb1c60a6983940531473b5fae7f936520adb
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-04-18 21:50:05 -0500

    There are 3 methods defined on Class, new, allocate and superclass

--- metaprogramming/monday/test_class.rb
+++ metaprogramming/monday/test_class.rb
@@ -4,4 +4,10 @@ class TestClass < Test::Unit::TestCase
   def test_class_as_object
     assert_equal(Class, String.class)
   end
+  def test_class_methods
+    inherited = false
+    assert_equal(true, Class.instance_methods(false).include?(:new))
+    assert_equal(true, Class.instance_methods(false).include?(:superclass))
+    assert_equal(true, Class.instance_methods(false).include?(:allocate))
+  end
 end
