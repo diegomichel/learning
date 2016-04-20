c1888e39e89276e1d975a1cd27b2faf8e3dea222
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-04-19 21:36:26 -0500

    Super Class of common Classes

--- metaprogramming/monday/test_class.rb
+++ metaprogramming/monday/test_class.rb
@@ -10,4 +10,11 @@ class TestClass < Test::Unit::TestCase
     assert_equal(true, Class.instance_methods(false).include?(:superclass))
     assert_equal(true, Class.instance_methods(false).include?(:allocate))
   end
+  def test_super_class
+    assert_equal(Object, String.superclass)
+    assert_equal(BasicObject, Object.superclass)
+    assert_equal(nil, BasicObject.superclass)
+    assert_equal(Module, Class.superclass)
+    assert_equal(Object, Module.superclass)
+  end
 end
