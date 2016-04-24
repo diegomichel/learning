44665b47beebd55354ae6bca9bb0a95effdeb7be
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-04-24 13:30:38 -0500

    The class of a class... is Class

---
+++ metaprogramming/monday/test_class_of_every_class.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+class MyClass; end
+class TestClassOfEveryClass < Test::Unit::TestCase
+  def test_the_class_of_every_class_must_be_Class
+    assert_equal(Class, String.class)
+    assert_equal(Class, Bignum.class)
+    assert_equal(Class, Integer.class)
+    assert_equal(Class, Fixnum.class)
+    assert_equal(Class, Float.class)
+    assert_equal(Class, MyClass.class)
+  end
+end
