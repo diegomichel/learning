0265dfc998e090269ff8d33de5fa8d5d316a74b7
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-05-08 21:04:33 -0500

    We are always inside of an object

--- metaprogramming/monday/self_test.rb
+++ metaprogramming/monday/self_test.rb
@@ -12,3 +12,9 @@ class SelfTest < Test::Unit::TestCase
     assert_equal('Banana', Banana.new.whoaim)
   end
 end
+
+# How can I test this?
+# We are always inside of an object
+puts "Test pass we are inside main on irb" if 'main' == self.to_s
+
+puts "The main object is an instance of Object" if Object == self.class
