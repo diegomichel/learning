641df758fe739c4f11d597263fc47c8ced01f2f1
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-05-12 20:16:39 -0500

    Send allows string or symbol

--- metaprogramming/tuesday/send_test.rb
+++ metaprogramming/tuesday/send_test.rb
@@ -10,4 +10,9 @@ class TestSendMethod < Test::Unit::TestCase
   def test_double_with_dot_and_send
     assert_equal(MyClass.new.double(2), MyClass.new.send(:double, 2))
   end
+
+  def test_send_with_symbol_and_string
+    object = MyClass.new
+    assert_equal(object.send(:double, 3), object.send('double', 3))
+  end
 end
