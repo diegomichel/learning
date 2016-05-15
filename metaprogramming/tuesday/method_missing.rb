174258c119fdd1a8f82f0ac7ffde416cddc80fd2
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-05-14 23:13:28 -0500

    Simple method missing playing around

---
+++ metaprogramming/tuesday/method_missing.rb
@@ -0,0 +1,17 @@
+class MyClass
+  def initialize
+    @data = {}
+  end
+
+  def method_missing(name, *args)
+    dato = name.to_s
+    if dato =~ /=$/
+      @data[dato.chop] = args[0]
+    else
+      @data[dato]
+    end
+  end
+end
+
+@my_object = MyClass.new
+@my_object.nombre=("Diego Michel")
