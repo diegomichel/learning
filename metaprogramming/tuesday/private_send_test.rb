b53ea3f012b67f82dfd4d5e3dc239b743f87082e
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-05-17 20:23:58 -0500

    It's expected that private methods raise exceptions when called by public_send

--- metaprogramming/tuesday/private_send_test.rb
+++ metaprogramming/tuesday/private_send_test.rb
@@ -11,4 +11,12 @@ class TestPrivacyMatters < Test::Unit::TestCase
   def test_private_with_send
     assert_equal('hello!', MyClass.new.send(:my_private_method))
   end
+  def test_private_method_with_public_send
+    begin
+      response = MyClass.new.public_send(:my_private_method)
+    rescue
+      response = false
+    end
+    assert_equal(false, response)
+  end
 end
