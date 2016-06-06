3a6cc91b13cc9d13217f62b0764d759ca3cc0662
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2016-06-05 23:29:26 -0500

    const_missing override test passing

--- metaprogramming/tuesday/const_missing_test.rb
+++ metaprogramming/tuesday/const_missing_test.rb
@@ -1,5 +1,9 @@
 require 'test/unit'
 
+def Object.const_missing(name)
+  name.to_s.downcase.gsub(/_/, ' ')
+end
+
 class TestConstMissing < Test::Unit::TestCase
   def test_const_missing
     assert_equal('my constant', MY_CONSTANT)
